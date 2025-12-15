4d5ba958fd3ced4044a17ffc6f0600bd8193fdc8
MeganWeijgers <754792@newcollege.ac.uk>
2025-12-15 01:45:39 +0000

    Player Movement

---
+++ Game jam 2025/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5F;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+        moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+    }
+}
